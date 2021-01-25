6cc987ee2c73db322d85cdf1b372e28f8fd5eb74
Scott Wilson <scottwilson1020@gmail.com>
2021-01-25 20:34:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,6 +28,7 @@ attr_accessor :sender, :receiver, :amount, :status, :count
     "Transaction rejected. Please check your account balance."
     end
   end
+  
  def reverse_transfer
     if @status == "complete"
       @sender.balance += @amount
